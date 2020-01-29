dee482aaeaf3fa4154c74ab4f2d65bc34d68b97e
nchuykin <nkchuykin@edu.hse.ru>
2020-01-29 18:42:09 +0300

    Добавлена задача H в шаблон третьего контеста

---
+++ Seminar3/TaskH/Hipster.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Hipster
+{
+    int depeartureDay;
+    int arrivalDay;
+
+    public int PostsRead { get; private set; }
+
+    public Hipster(int depeartureDay, int arrivalDay)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ReadPost(DateTime date)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Subscribe(Blogger blogger)
+    {
+        throw new NotImplementedException();
+    }
+}
+
---
+++ Seminar3/TaskH/Program.cs
@@ -0,0 +1,47 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int hipstersCount = int.Parse(Console.ReadLine());
+        int bloggersCount = int.Parse(Console.ReadLine());
+        Hipster[] hipsters = new Hipster[hipstersCount];
+        Blogger[] bloggers = new Blogger[bloggersCount];
+        Calendar calendar = new Calendar();
+        for (int i = 0; i < bloggersCount; i++)
+        {
+            bloggers[i] = new Blogger((DayOfWeek)int.Parse(Console.ReadLine()));
+            calendar.EveryDayNotification += bloggers[i].MakePost;
+        }
+        ReadHipsters(hipstersCount, hipsters, bloggers);
+        calendar.Simulate(new DateTime(2019, 1, 1), new DateTime(2019, 12, 31));
+        PrintResult(hipsters);
+    }
+
+    private static void PrintResult(Hipster[] arr)
+    {
+        foreach (Hipster hipster in arr)
+            Console.WriteLine(hipster.PostsRead);
+    }
+
+    private static void ReadHipsters(int hipstersCount, Hipster[] hipsters, Blogger[] bloggers)
+    {
+        for (int i = 0; i < hipstersCount; i++)
+        {
+            string[] arr = Console.ReadLine().Split();
+            hipsters[i] = new Hipster(int.Parse(arr[0]), int.Parse(arr[1]));
+            for (int j = 2; j < arr.Length; j++)
+            {
+                hipsters[i].Subscribe(bloggers[int.Parse(arr[j])]);
+            }
+        }
+    }
+}
+
